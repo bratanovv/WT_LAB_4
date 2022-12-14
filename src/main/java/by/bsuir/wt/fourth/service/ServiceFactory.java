e756a2949c749a1e51085fa8a5c02c211dcfff74
bratanovv <bratanov.misha@mail.ru>
2022-12-15 01:34:26 +0300

    Initial commit

---
+++ src/main/java/by/bsuir/wt/fourth/service/ServiceFactory.java
@@ -0,0 +1,47 @@
+package by.bsuir.wt.fourth.service;
+
+import by.bsuir.wt.fourth.service.api.*;
+import by.bsuir.wt.fourth.service.impl.*;
+
+public class ServiceFactory {
+
+    private ServiceFactory() {
+    }
+
+    public static ServiceFactory getInstance() {
+        return Holder.INSTANCE;
+    }
+
+
+
+    private final UserOrderService userOrderService=new UserOrderServiceImpl();
+    private final UserInformationService userInformationService=new UserInformationServiceImpl();
+    private final ApartmentService apartmentService=new ApartmentServiceImpl();
+    private final UserService userService=new UserServiceImpl();
+    private final RoleService roleService=new RoleServiceImpl();
+
+
+    public ApartmentService getApartmentService() {
+        return apartmentService;
+    }
+
+    public RoleService getRoleService() {
+        return roleService;
+    }
+
+    public UserService getUserService() {
+        return userService;
+    }
+
+    public UserOrderService getUserOrderService() {
+        return userOrderService;
+    }
+
+    public UserInformationService getUserInformationService() {
+        return userInformationService;
+    }
+
+    private static class Holder {
+        static final ServiceFactory INSTANCE = new ServiceFactory();
+    }
+}
